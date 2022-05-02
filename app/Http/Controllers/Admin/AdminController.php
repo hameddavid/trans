2900656f12389255f46b08899298a1de98a7ad21
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-02 13:43:18 +0100

    Create AdminController.php

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Application;
+
+class AdminController extends Controller
+{
+    public function viewPendingApplications(Request $request){
+        $data = [];
+        $apps = Application::where('app_status','pending')->select('*')->get(); 
+        return view('pages.pending_requests',['data'=>$data,'apps'=>$apps]);
+    }
+}
