1d8b22823b491b6c9a839a4a051cbed569688d42
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-15 09:06:37 +0100

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -208,7 +208,7 @@
                                                         <td>{{$app->matric_number}}</td>
                                                         <td>@php echo ($app->app_status == 'APPROVED') ? '<span class="badge badge-soft-success">'.$app->app_status.'</span>' : '<span class="badge badge-soft-danger">'.$app->app_status.'</span>'@endphp</td>
                                                         <td>
-                                                            <button type="button" class="btn btn-light btn-sm">View</button>
+                                                            <button type="button" class="btn btn-light btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModalFullscreen">View</button>
                                                         </td>
                                                     </tr>
                                                     @if ($i > 5)
@@ -278,7 +278,55 @@
 
                 @include("partials.footer")
             </div>
-            <!-- end main content-->        
+            <!-- end main content-->     
+            
+            <!-- sample modal content -->
+            <div id="exampleModalFullscreen" class="modal fade" tabindex="-1" aria-labelledby="exampleModalFullscreenLabel" aria-hidden="true">
+                <div class="modal-dialog modal-fullscreen">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalFullscreenLabel">Fullscreen Modal</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h5>Overflowing text to show scroll behavior</h5>
+                            <p>Cras mattis consectetur purus sit amet fermentum.
+                                Cras justo odio, dapibus ac facilisis in,
+                                egestas eget quam. Morbi leo risus, porta ac
+                                consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque
+                                nisl consectetur et. Vivamus sagittis lacus vel
+                                augue laoreet rutrum faucibus dolor auctor.</p>
+                            <p>Aenean lacinia bibendum nulla sed consectetur.
+                                Praesent commodo cursus magna, vel scelerisque
+                                nisl consectetur et. Donec sed odio dui. Donec
+                                ullamcorper nulla non metus auctor
+                                fringilla.</p>
+                            <p>Cras mattis consectetur purus sit amet fermentum.
+                                Cras justo odio, dapibus ac facilisis in,
+                                egestas eget quam. Morbi leo risus, porta ac
+                                consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque
+                                nisl consectetur et. Vivamus sagittis lacus vel
+                                augue laoreet rutrum faucibus dolor auctor.</p>
+                            <p>Aenean lacinia bibendum nulla sed consectetur.
+                                Praesent commodo cursus magna, vel scelerisque
+                                nisl consectetur et. Donec sed odio dui. Donec
+                                ullamcorper nulla non metus auctor
+                                fringilla.</p>
+                            <p>Cras mattis consectetur purus sit amet fermentum.
+                                Cras justo odio, dapibus ac facilisis in,
+                                egestas eget quam. Morbi leo risus, porta ac
+                                consectetur ac, vestibulum at eros.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary waves-effect" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary waves-effect waves-light">Save changes</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
 
         <!-- apexcharts -->
         <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
@@ -288,6 +336,7 @@
         <script src="assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
         <!-- dashboard init -->
         <script src="assets/js/pages/dashboard.init.js"></script>
+        <script src="assets/js/pages/modal.init.js"></script>
     @endsection
 
         
