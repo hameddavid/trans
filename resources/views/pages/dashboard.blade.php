528b05ef18d943c6d3c2d0b6cbf40790f2d0d2ba
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-16 00:48:36 +0100

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -260,6 +260,8 @@
                                                         <td>@php echo ($payment->status_msg == 'success') ? '<span class="badge badge-soft-success">'.$payment->status_msg.'</span>' : '<span class="badge badge-soft-danger">'.$payment->status_msg.'</span>'@endphp</td>
                                                     </tr>
                                                     @endforeach
+                                                    @if($data->role == 200)<button id="btnRecommend" type="button" class="btn btn-primary waves-effect waves-light">Recommend</button>@endif
+                            @if($data->role == 300)<button id="btnApprove" type="button" class="btn btn-primary waves-effect waves-light">Approve</button>@endif
                                                 </tbody>
                                             </table>
                                         </div>
@@ -292,8 +294,7 @@
                         <div class="modal-footer">
                             
                             <button type="button" class="btn btn-secondary waves-effect" data-bs-dismiss="modal">Close</button>
-                            <!-- @if($data->role == 200)<button id="btnRecommend" type="button" class="btn btn-primary waves-effect waves-light">Recommend</button>@endif
-                            @if($data->role == 300)<button id="btnApprove" type="button" class="btn btn-primary waves-effect waves-light">Approve</button>@endif -->
+                            
                         </div>
                     </div>
                 </div>
