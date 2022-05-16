8acfb69fd46e5f4f9b77160e41f3366b5f8be5a6
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-16 01:14:21 +0100

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -288,11 +288,7 @@
                         </div>
                         <div class="modal-body showHTML">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary waves-effect" data-bs-dismiss="modal">Close</button>
-                            @if($data->role == 200)<button id="btnRecommend" type="button" class="btn btn-primary waves-effect waves-light">Recommend</button>@endif
-                            @if($data->role == 300)<button id="btnApprove" type="button" class="btn btn-primary waves-effect waves-light">Approve</button>@endif
-                        </div>
+                        
                     </div>
                 </div>
             </div>
