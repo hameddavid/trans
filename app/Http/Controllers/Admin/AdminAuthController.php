75c2fec311d98c367bb8c3bc66dca0fce59c5579
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-16 19:40:42 +0100

    Update AdminAuthController.php

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -247,6 +247,7 @@ class AdminAuthController extends Controller
    public function admin_reset_password(Request $request){
     $request->validate(['old_pass'=>'required', 'password'=>'required',]);
     $data =  $this->auth_user(session('user'));
+    dd($data);
     try {
   
     $app = Admin::where('email',$data->email)->first();
