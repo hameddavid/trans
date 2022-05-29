551f0432311accf2e194bcbf9d3343d2da01a944
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-29 13:44:35 +0100

    Create proficiency_letter.blade.php

---
+++ resources/views/proficiency_letter.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <style type="text/css">
+            .bodyBody {
+                margin: 10px;
+                font-size: 1.50em;
+            }
+            .divHeader {
+                text-align: right;
+                border: 1px solid;
+            }
+            .divReturnAddress {
+                text-align: left;
+                float: right;
+            }
+            .divSubject {
+                clear: both;
+                font-weight: bold;
+                padding-top: 80px;
+            }
+            .divAdios {
+                float: left;
+                padding-top: 50px;
+            }
+        </style>
+    </head>
+    <body class="bodyBody">
+        <div class="divSubject">
+<pre>
+RUN/REG/Attestation/15/Vol.1/{014}                                                  {12th April, 2022}                                                         
+
+TO WHOM IT MAY CONCERN
+</pre>
+        </div>
+
+        <div class="divContents" align="justify">
+            <p>
+                Dear Sir,
+            </p>
+            <h5>
+                <u>LETTER OF ATTESTATION<br>
+                    LANGUAGE OF INSTRUCTION/PROFICIENCY IN ENGLISH                    
+                </u>
+            </h5>
+            
+            <p>
+                <p>{Miss ALADEBO, Kikelomo Josephine} (Matric. No. {RUN08-09/1626}) was a student in the Department of {Finance (Banking and Finance Programme)} in the Faculty of {Management Sciences}, Redeemer’s University.</p>
+
+                <p>{Her} Cumulative Grade Point Average (CGPA) at the end of a {4-year} {Bachelor of Science} degree programme, in the {2009/2010} academic session, in {Psychology} was {3.52} – {Second Class (Honours) Upper Division}.</p>
+
+                <p>Kindly note that English is the medium of communication in Nigerian institutions. You may also wish to note that in Redeemer’s University, all lectures, examinations, tests,
+                    Seminars, presentations, and all kinds of student assessments are conducted in English.</p>
+
+                <p>Please accord her the necessary assistance.</p>
+
+                Yours faithfully,
+
+            </p>
+        </div>
+
+        <div class="divAdios">
+            MISS ADETUTU ADEWOLE<br>
+            Administrative Officer, Academic Affairs<br>
+            For:  REGISTRAR
+        </div>
+    </body>
+</html>
