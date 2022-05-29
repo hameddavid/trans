d35745b7b3c4c23508d5da89aea70eee81f8d87d
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-29 13:38:29 +0100

    cover letters

---
+++ resources/views/cover_letter.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <style type="text/css">
+            .bodyBody {
+                margin: 10px;
+                font-size: 1.50em;
+            }
+            .divHeader {
+                text-align: right;
+                border: 1px solid;
+            }
+            .divReturnAddress {
+                text-align: left;
+                float: right;
+            }
+            .divSubject {
+                clear: both;
+                font-weight: bold;
+                padding-top: 80px;
+            }
+            .divAdios {
+                float: left;
+                padding-top: 50px;
+            }
+        </style>
+    </head>
+    <body class="bodyBody">
+        <div class="divSubject">
+<pre>
+RUN/REG/Acad/Stud.Trscpt/53/Vol.12/{014}                                               {12th April, 2022}                                                         
+{University of Sussex
+Sussex House
+Brighton, BN1 9RH
+United Kingdom}
+</pre>
+        </div>
+
+        <div class="divContents" align="justify">
+            <p>
+                Dear Sir,
+            </p>
+            <h5>
+                <u>TRANSCRIPT OF {MISS OKANLAWON MORENIKEJI AYOTUNDE} AND ATTESTATION TO PROFICIENCY IN ENGLISH LANGUAGE<br>
+                    {REFERENCE NUMBER: 216858079}
+                </u>
+            </h5>
+            
+            <p>
+                <p>Please find forwarded herewith, the transcript of {Miss OKANLAWON, Morenikeji Ayotunde} (Matric. No. {RUN06-07/878}).</p>
+
+                <p>{Her} Cumulative Grade Point Average (CGPA) at the end of a {4-year} {Bachelor of Science} degree programme, in the {2009/2010} academic session, in {Psychology} was {3.52} – {Second Class (Honours) Upper Division}. The official language of teaching and examining the course was English.</p>
+
+                <p>Kindly note that any alteration on the transcript renders the records invalid and that the transcript is being forwarded in strict confidence and under no circumstance should it be released to the applicant.</p>
+
+                <p>I hope you will find the academic records useful.</p>
+
+                Yours faithfully,
+
+            </p>
+        </div>
+
+        <div class="divAdios">
+            D. K. T. Akintola<br>
+            Deputy Registrar, Academic Affairs<br>
+            For:  REGISTRAR
+        </div>
+    </body>
+</html>
---
+++ resources/views/verification.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <style type="text/css">
+            .bodyBody {
+                margin: 10px;
+                font-size: 1.50em;
+            }
+            .divHeader {
+                text-align: right;
+                border: 1px solid;
+            }
+            .divReturnAddress {
+                text-align: left;
+                float: right;
+            }
+            .divSubject {
+                clear: both;
+                font-weight: bold;
+                padding-top: 80px;
+            }
+            .divAdios {
+                float: left;
+                padding-top: 50px;
+            }
+            table {
+                font-family: sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+            td, th{
+                border: 1px solid #dddddd;
+                padding: 8px;
+            }
+        </style>
+    </head>
+    <body class="bodyBody">
+        <div class="divSubject">
+<pre>
+RUN/REG/Acad/Verifi/63/Vol.2/{627}                                                  {12th April, 2022}                                                         
+{Prince House
+53-54 Queens Road
+Brighton, BN1 3XB
+United Kingdom}
+
+{Attention: JP Morgan Chase}
+</pre>
+        </div>
+
+        <div class="divContents" align="justify">
+            <p>
+                Dear Sir,
+            </p>
+            <h5>
+                <u>{RE: REFERENCE REQUEST FOR EMMANUEL OGBONNA [JPM09761]}</u>
+            </h5>
+            
+            <p>
+                <p>I write to acknowledge receipt of your email dated {21st December, 2021} in connection with the above-mentioned subject and verify that the under-mentioned person was admitted to the Redeemer’s University to study for a degree course leading to the award of {Bachelor of Science (BSc.)} as summarised below:</p>
+
+
+                <p>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>S/N</th>
+                                <th>Name</th>
+                                <th>Year of Admission</th>
+                                <th>Course of Study</th>
+                                <th>Class of Degree</th>
+                                <th>Year of Graduation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>{Name}</td>
+                                <td>{Year of Admission}</td>
+                                <td>{Course of Study}</td>
+                                <td>{Class of Degree}</td>
+                                <td>{Year of Graduation}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </p>
+
+                <p>I hope you will find the above information useful.</p>
+
+                Yours faithfully,
+
+            </p>
+        </div>
+
+        <div class="divAdios">
+            D. K. T. Akintola<br>
+            Deputy Registrar, Academic Affairs<br>
+            For:  REGISTRAR
+        </div>
+    </body>
+</html>
