7f7a23748b337fad225026cf496609282e6529c8
hameddavid <reganalyst@yahoo.com>
2022-06-01 17:38:27 +0100

    Adjustment on new account creation..

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -367,8 +367,9 @@ public function get_delivery_msg($data){
 public function send_corrections_to_applicant(Request $request){
     $request->validate(['appid'=>'required',]);
     //try {
-    collect($request->all())->filter();
-    $form_data = $request->except(['appid','_token']);
+    $new_req = collect($request->all())->filter();
+    $form_data = $new_req->except(['appid','_token']);
+    dd($form_data);
     $form_array = [];
     $edit_token = app('App\Http\Controllers\Applicant\ApplicantAuthController')::RandomString(6);
     $msg ='<span style="color:red"> Use token '.$edit_token. ' to edit your application.<span><br><br>';
