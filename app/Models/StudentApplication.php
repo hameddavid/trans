b4d921cc59135f234545dcc932d307af286744de
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-06-02 12:55:28 +0100

    Update StudentApplication.php

--- app/Models/StudentApplication.php
+++ app/Models/StudentApplication.php
@@ -9,4 +9,7 @@ class StudentApplication extends Model
 {
     use HasFactory;
     protected $table = 'student_applications';
+    protected $casts = [
+        'created_at' => 'datetime:m/d/Y'
+    ];
 }
