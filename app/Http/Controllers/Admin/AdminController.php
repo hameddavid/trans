4e0759870e72bcf5ec7072a684a136119014c2e6
hameddavid <reganalyst@yahoo.com>
2022-06-04 15:03:56 +0100

    Header image

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -297,10 +297,16 @@ class AdminController extends Controller
         //#################################################################################
                $pdf = new \Dompdf\Dompdf();
                $pdf->set_option('isRemoteEnabled',TRUE);
-               $pdf->loadHtml(view('cover_letter',['data'=> $app_official]))->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'_cover.pdf');
+               $pdf->loadHtml(view('cover_letter',['data'=> $app_official]));
+               $pdf->setPaper('a4', 'portrate');
+               $pdf->setWarnings(false);
+               $pdf->save($app_official->used_token.'_cover.pdf');
                $pdf = new \Dompdf\Dompdf();
                $pdf->set_option('isRemoteEnabled',TRUE);
-               $pdf->loadHtml(view('result',['data'=> $app_official->transcript_raw]))->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'.pdf');
+               $pdf->loadHtml(view('result',['data'=> $app_official->transcript_raw]));
+               $pdf->setPaper('a4', 'portrate');
+               $pdf->setWarnings(false);
+               $pdf->save($app_official->used_token.'.pdf');
             if (File::exists($app_official->used_token.'.pdf') && File::exists($app_official->used_token.'_cover.pdf')
             && File::exists( storage_path('app/'.$app_official->certificate)) ) {
                 if(strtoupper($app_official->delivery_mode) == "SOFT"){
