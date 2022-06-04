2361a96cb2190e9c2e8074f66234e66bcb25f17a
hameddavid <reganalyst@yahoo.com>
2022-06-04 14:49:45 +0100

    Header image

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -290,19 +290,17 @@ class AdminController extends Controller
             ->where(['application_id'=> $request->id, 'app_status'=>'RECOMMENDED'])->select('official_applications.*','official_applications.used_token AS file_path','applicants.surname','applicants.firstname','applicants.email','applicants.sex','applicants.id')->first(); 
             if($app_official){
                  //############ Permitir ver imagenes si falla ################################
-          $contxt = stream_context_create([
-            'ssl' => ['verify_peer' => FALSE,'verify_peer_name' => FALSE,'allow_self_signed' => TRUE, ] ]);
-        $pdf = PDF::setOptions(['isHTML5ParserEnabled' => true, 'isRemoteEnabled' => true]);
-        $pdf->getDomPDF()->setHttpContext($contxt);
+        //   $contxt = stream_context_create([
+        //     'ssl' => ['verify_peer' => FALSE,'verify_peer_name' => FALSE,'allow_self_signed' => TRUE, ] ]);
+        // $pdf = PDF::setOptions(['isHTML5ParserEnabled' => true, 'isRemoteEnabled' => true]);
+        // $pdf->getDomPDF()->setHttpContext($contxt);
         //#################################################################################
-        $pdf->loadView('cover_letter',['data'=> $app_official])->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'_cover.pdf');
-          //############ Permitir ver imagenes si falla ################################
-          $contxt = stream_context_create([
-            'ssl' => ['verify_peer' => FALSE,'verify_peer_name' => FALSE,'allow_self_signed' => TRUE, ] ]);
-        $pdf = PDF::setOptions(['isHTML5ParserEnabled' => true, 'isRemoteEnabled' => true]);
-        $pdf->getDomPDF()->setHttpContext($contxt);
-        //#################################################################################
-        $pdf->loadView('result',['data'=> $app_official->transcript_raw])->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'.pdf');
+               $pdf = new PDF;
+               $pdf->set_option('isRemoteEnabled',TRUE);
+               $pdf->loadView('cover_letter',['data'=> $app_official])->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'_cover.pdf');
+               $pdf = new PDF;
+               $pdf->set_option('isRemoteEnabled',TRUE);
+               $pdf->loadView('result',['data'=> $app_official->transcript_raw])->setPaper('a4', 'portrate')->setWarnings(false)->save($app_official->used_token.'.pdf');
             if (File::exists($app_official->used_token.'.pdf') && File::exists($app_official->used_token.'_cover.pdf')
             && File::exists( storage_path('app/'.$app_official->certificate)) ) {
                 if(strtoupper($app_official->delivery_mode) == "SOFT"){
