f021bbf80b54b55b82ef2bff8a7d952d55e6009b
hameddavid <reganalyst@yahoo.com>
2022-06-04 03:21:34 +0100

    View Cert

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -21,9 +21,9 @@ use URL;
 
 class AdminController extends Controller
 {
-    public function __construct()
+    public function __construct() 
     {
-        // $this->middleware('adminauth');
+         $this->middleware('adminauth');
         // $this->middleware('Adminauth',['only' => ['password_reset','applicant_dashboard']]);
        // $this->middleware('log')->only('index');
        // $this->middleware('subscribed')->except('store');
@@ -31,6 +31,7 @@ class AdminController extends Controller
 
     public function view_certificate($path){
        // $filepath = storage_path($path); 
+       return Storage::url($path);
        $s_path = storage_path('app/credentials/'.$path); //. '/app/' . $path ;
        $file = File::get($s_path);
        if(!File::exists($s_path)) abort(404);
