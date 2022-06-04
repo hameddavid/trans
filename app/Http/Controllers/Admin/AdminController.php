4b4952b52d432e1624dd10d94b4b7c83cc97efbf
hameddavid <reganalyst@yahoo.com>
2022-06-04 02:38:18 +0100

    View Cert

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -31,6 +31,10 @@ class AdminController extends Controller
 
     public function view_certificate($path){
        // $filepath = storage_path($path); 
+       $s_path = storage_path('app/credentials/'.$path); //. '/app/' . $path ;
+       $file = File::get($s_path);
+       if(!File::exists($s_path)) abort(404);
+       return "Exist";
         return response()->download(storage_path('app/credentials/'.$path), null, [], null);
        // return response()->file($filepath);
     }
