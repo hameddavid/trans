b512bfddd9ee801b9e77c009851ab3ceb1cea069
hameddavid <reganalyst@yahoo.com>
2022-06-04 05:58:10 +0100

    View Cert

--- app/Mail/MailingOfficialSoft.php
+++ app/Mail/MailingOfficialSoft.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MailingApplicant extends Mailable
+class MailingOfficialSoft extends Mailable
 {
     use Queueable, SerializesModels;
 
