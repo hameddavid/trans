e15cd72a4a3330c73d44e81a82b1c546e8f6558d
hameddavid <reganalyst@yahoo.com>
2022-06-20 19:15:59 +0100

    Edit again

--- app/Http/Controllers/Applicant/ApplicationController.php
+++ app/Http/Controllers/Applicant/ApplicationController.php
@@ -154,6 +154,7 @@ class ApplicationController extends Controller
                             ->select('student_applications.*','student_applications.address AS file_path','applicants.surname','applicants.firstname','applicants.email','applicants.sex')->first(); 
                             PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])->loadView('proficiency_letter',['data'=> $app_stud])->setPaper('a4', 'portrate')->setWarnings(false)->save($app_stud->file_path.'.pdf');
                         }
+                        return $app_stud;
                         // Notify applicant through email  $applicant->email and Notify admin
                         $Subject= $type." APPLICATION NOTIFICATION";
                        if(app('App\Http\Controllers\Applicant\ConfigController')->applicant_mail($applicant,$Subject,$Msg=$this->get_msg2())['status'] == 'ok'){
