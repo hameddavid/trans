8e4ae91f0a6a6b137c290fad3c4b848073156791
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-06-27 13:51:56 +0100

    update

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -299,16 +299,16 @@
             <!-- /modal -->
 
         <!-- apexcharts -->
-        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+        <script src="../assets/libs/apexcharts/apexcharts.min.js"></script>
 
         <!-- Plugins js-->
-        <script src="assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
-        <script src="assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="../assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="../assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
         <!-- dashboard init -->
-        <script src="assets/js/pages/dashboard.init.js"></script>
-        <script src="assets/js/pages/modal.init.js"></script>
-        <script src="assets/js/validation.min.js"></script>
-        <script src="assets/js/utils.js"></script>
+        <script src="../assets/js/pages/dashboard.init.js"></script>
+        <script src="../assets/js/pages/modal.init.js"></script>
+        <script src="../assets/js/validation.min.js"></script>
+        <script src="../assets/js/utils.js"></script>
     @endsection
 
         
