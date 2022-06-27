bc92c6b4769f18c172cabd05796b9407e0640e0c
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-06-27 18:56:10 +0100

    update

--- public/assets_records/js/functions.js
+++ public/assets_records/js/functions.js
@@ -5,8 +5,10 @@ $(document).ready(function () {
     $("#doc_type").change(function () {
         if ($("#doc_type").val() === "transcript") {
             $(".transcript").show();
+            $(".degree").hide();
         } else if ($("#doc_type").val() === "degree") {
             $(".degree").show();
+            $(".transcript").hide();
         } else {
             $(".transcript").hide();
             $(".degree").hide();
