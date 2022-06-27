235f40a01384f12084621689e2c970455723d90d
hameddavid <reganalyst@yahoo.com>
2022-06-27 12:43:38 +0100

    LetterHead

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -3,6 +3,22 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use App\Models\OfficialApplication;
+use App\Models\StudentApplication;
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Payment;
+use App\Models\Applicant;
+use App\Models\RegistrationResult;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Mail;
+use App\Mail\MailingAdmin;
+use App\Mail\MailingApplicant;
+use PDF;
+
 
 class RecordController extends Controller
 {
@@ -23,7 +39,7 @@ class RecordController extends Controller
         $validator = Validator::make($request, [ 'used_token' => 'required|string',"matno"=>"required"]);
         if ($validator->fails()) {  return response(['status'=>'failed','message'=>'Verification code/Matric number are required!'],401);  }
        
-        $app_stud = StudentApplication::join('applicants', 'student_applications.applicant_id', '=', 'applicants.id')
+        $app_stud = OfficialApplication::join('applicants', 'student_applications.applicant_id', '=', 'applicants.id')
             ->where(['student_applications.matric_number'=> $request->matno,'student_applications.used_token'=> $request->used_token])
             ->select('student_applications.*','student_applications.address AS file_path','applicants.surname','applicants.firstname','applicants.email','applicants.sex')->first(); 
         if($app_stud->count() == 1){
