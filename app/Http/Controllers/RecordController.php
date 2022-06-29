178e17637d068e6b0a733366193063f5b4e2e926
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-06-29 05:14:01 +0100

    update

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -38,8 +38,8 @@ class RecordController extends Controller
 
 
     public function transcript_verification(Request $request){
-        dd($request->all());
-        $validator = Validator::make($request, ["used_token" => "required|string","matno"=>"required"]);
+        //$validator = Validator::make($request, ["used_token" => "required|string","matno"=>"required"]);
+        $request->validate(["used_token" => "required|string","matno"=>"required"]);
         if ($validator->fails()) {  return response(['status'=>'failed','message'=>'Verification code/Matric number are required!'],401);  }
        
         $app_stud = OfficialApplication::join('applicants', 'student_applications.applicant_id', '=', 'applicants.id')
