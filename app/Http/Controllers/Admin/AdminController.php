caa1c603c18e34d124749dd9420bec637568d614
hameddavid <reganalyst@yahoo.com>
2022-06-29 13:13:53 +0100

    LetterHead

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -56,7 +56,7 @@ class AdminController extends Controller
 
     public function view_certificate($path){
        
-        if (File::exists($path.'.pdf')){
+        if (File::exists($path)){
         $s_path = storage_path('app/credentials/'.$path);
         return Response::make(file_get_contents($s_path), 200, [
             'Content-Type' => 'application/pdf',
@@ -66,7 +66,7 @@ class AdminController extends Controller
     } else{ return back();}
     }
 
-    
+
     public function view_proficiency($path){
         $s_path = public_path($path);  
         if (File::exists($path.'.pdf')){
