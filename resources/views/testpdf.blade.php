27a4860a3a0755b9f889b27cd872f79dc09790a8
hameddavid <reganalyst@yahoo.com>
2022-07-02 17:02:21 +0100

    Degree

---
+++ resources/views/testpdf.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    
+
+    <img src="{{'C:\xampp8.1.2_PHP 8.1.2\htdocs\transcript\public/assets/images/run_logo_big.png'}}">
+    
+   
+</body>
+</html>
