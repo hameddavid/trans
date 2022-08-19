165ac4cf220a4afbb4bfec7a3fa588a1c6a994ef
hameddavid <reganalyst@yahoo.com>
2022-08-19 18:22:43 +0100

    Now

--- app/Http/Controllers/Applicant/ApplicationController.php
+++ app/Http/Controllers/Applicant/ApplicationController.php
@@ -170,7 +170,7 @@ class ApplicationController extends Controller
                             ->where(['student_applications.id'=> $new_application->id, 'app_status'=>'PENDING'])
                             ->select('student_applications.*','student_applications.address AS file_path','applicants.surname','applicants.firstname','applicants.email','applicants.sex')->first();   
                             $pdf = PDF::loadView('proficiency_letter',['data'=> $app_stud]); 
-                            File::put($app_stud->file_path.'.pdf', $pdf->output()); 
+                            // File::put($app_stud->file_path.'.pdf', $pdf->output()); 
                 
                         }  
                         // Notify applicant through email  $applicant->email and Notify admin
