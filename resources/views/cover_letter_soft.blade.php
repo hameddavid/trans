44fa7e5a92f1aa90471aa510adbab619da957d71
hameddavid <reganalyst@yahoo.com>
2022-08-27 06:36:22 +0100

    PDF image issue

--- resources/views/cover_letter_soft.blade.php
+++ resources/views/cover_letter_soft.blade.php
@@ -17,12 +17,13 @@
             }
             
             .bodyBody {
-                font-family: Arial;
-                font-size: 11px;
-                background-image: url("https://records.run.edu.ng/assets/images/original.jpg");
+                background-image: url("https://records.run.edu.ng/assets/images/metal_logo.png");
+                /* background-image: url("https://records.run.edu.ng/assets/images/original.jpg"); */
                 /* background-image: url('/www/wwwroot/trans/public/assets/images/original.jpg'); */
                 background-size: contain;
                 background-repeat: no-repeat;
+                font-family: Arial;
+                font-size: 11px;
 
             }
             .divHeader {
